9007c0d092a0936f41269af17c0890243f924b74
rachid ouhammou <rachidouhammou21@gmail.com>
2023-01-19 21:40:52 +0100

    initialise the project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spring boot Project
+
+this repo is website made by `Spring boot` framework and `react`.
+
+to clone this repo and contribution to it :
+```
+git clone https://github.com/ouhammmourachid/spring-boot-project.git
+```
---
+++ src/test/java/com/web/springbootproject/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
