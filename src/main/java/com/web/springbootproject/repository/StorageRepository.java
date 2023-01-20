5f23b23993a74fcae2ebd3a71ae05ad1a9a9253b
rachid ouhammou <rachidouhammou21@gmail.com>
2023-01-20 14:20:23 +0100

    changing the name of the entity so that can support many types of documents.

--- src/main/java/com/web/springbootproject/repository/StorageRepository.java
+++ src/main/java/com/web/springbootproject/repository/StorageRepository.java
@@ -1,10 +1,10 @@
 package com.web.springbootproject.repository;
 
-import com.web.springbootproject.entity.ImageData;
+import com.web.springbootproject.entity.FileData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface StorageRepository extends JpaRepository<ImageData,Long> {
-    Optional<ImageData> findByName(String fileName);
+public interface StorageRepository extends JpaRepository<FileData,Long> {
+    Optional<FileData> findByName(String fileName);
 }
--- src/main/java/com/web/springbootproject/service/StorageService.java
+++ src/main/java/com/web/springbootproject/service/StorageService.java
@@ -1,8 +1,8 @@
 package com.web.springbootproject.service;
 
 import com.web.springbootproject.repository.StorageRepository;
-import com.web.springbootproject.entity.ImageData;
-import com.web.springbootproject.util.ImageUtils;
+import com.web.springbootproject.entity.FileData;
+import com.web.springbootproject.util.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,20 +20,21 @@ public class StorageService {
     }
 
     public String uploadImage(MultipartFile file) throws IOException {
-        ImageData imageData = storageRepository.save(
-                ImageData.builder()
+        FileData imageData = storageRepository.save(
+                FileData.builder()
                         .name(file.getOriginalFilename())
                         .type(file.getContentType())
-                        .imageData(ImageUtils.compressImage(file.getBytes()))
+                        .fileData(FileUtils.compressImage(file.getBytes()))
                         .build());
+
         if(imageData!=null){
-            return "image uploaded sucessfully :"+file.getOriginalFilename();
+            return "image uploaded successfully :"+file.getOriginalFilename();
         }
         return null;
     }
     public byte[] downloadImage(String fileName){
-        Optional<ImageData> imageData = storageRepository.findByName(fileName);
-        byte[] image = ImageUtils.decompressImage(imageData.get().getImageData());
-        return image;
+        Optional<FileData> imageData = storageRepository.findByName(fileName);
+        byte[] file = FileUtils.decompressImage(imageData.get().getFileData());
+        return file;
     }
 }
